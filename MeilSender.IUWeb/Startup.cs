0881a8a3d0eeb9c79da7e22d2c533fea530e6c03
Julio Olguin <Julio.Olguin@ipsos.com>
2018-11-27 20:01:37 -0600

    MailSender

---
+++ MeilSender.IUWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeilSender.IUWeb.Startup))]
+namespace MeilSender.IUWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
